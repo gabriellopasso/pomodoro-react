767caec1efa8f68a3dc89cc4a11fd4c56e1f7508
Gabriel Lopasso <gabriellopasso@gmail.com>
2025-06-09 22:17:47 -0300

    First component react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//PascalCase
+//App
+//HeaderHeading
+import { Heading } from './components/Heading';
+import './styles/theme.css';
+import './styles/goblal.css';
+import { TimerIcon } from 'lucide-react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading>
+        Sigismubndo{' '}
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quae
+        iste eius distinctio natus assumenda doloremque facere a iure aliquam,
+        architecto cupiditate quaerat vel aperiam quasi nulla fugit voluptatibus
+        illo.
       </p>
     </>
-  )
+  );
 }
-
-export default App
